221526adbbb261e76936b918ae617720c19a0ae0
Wojtek Okoński <wojteko22.96@gmail.com>
2017-10-10 09:31:32 +0200

    Add watching changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 
-gulp.task('html', function(){
+gulp.task('pug', function(){
     return gulp.src('templates/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('build/html'))
 });
 
-gulp.task('default', [ 'html' ]);
+gulp.task('watch', function() {
+    gulp.watch('templates/**/*.pug', ['pug']);
+});
+
+gulp.task('default', [ 'watch', 'pug' ]);
