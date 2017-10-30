afb537d3733ed6521402007a39f2f865d3a7780f
Wojtek Okoński <wojteko22.96@gmail.com>
2017-10-30 18:01:33 +0100

    4 Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('pug', function(){
     return gulp.src('src/pug/*.pug')
@@ -11,6 +12,10 @@ gulp.task('pug', function(){
 gulp.task('sass', function(){
     return gulp.src('src/sass/*.sass')
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('dist/css/'))
 });
 
