8dcc667e5c13b34de4d2dcad37b3b987bc716008
Dawid Rusiecki <dawid.rusiecki@fingo.info>
2017-11-05 21:03:53 +0100

    5 while

--- dist/js/memes.js
+++ dist/js/memes.js
@@ -25,19 +25,47 @@
         var answers = ["monachium", "pazdan", "marina"];
         var points = 0;
         for (var i = 0; i < questions.length; i++) {
-            var answer;
-            do {
-                answer = window.prompt(questions[i]);
-            } while (!answer);
-            if (answer.toLowerCase() === answers[i]) {
-                window.alert("Dobra odpowiedź!");
-                points++;
-            } else {
-                window.alert("Zła odpowiedź!");
+            var answer = showQuestion(questions[i]);
+            var isCorrect = checkAnswer(answers[i], answer);
+            points += isCorrect? 1 : 0;
+            if (i === 1 && !isCorrect){
+                pazdanFight(questions[i], answers[i])
             }
         }
+        checkResult(points);
+    }
+
+    function showQuestion(question) {
+        var answer;
+        while (!answer) {
+            answer = window.prompt(question);
+        }
+        return answer;
+    }
+
+    function checkAnswer(properAnswer, answer) {
+        if (answer.toLowerCase() === properAnswer) {
+            window.alert("Dobra odpowiedź!");
+        } else {
+            window.alert("Zła odpowiedź!");
+        }
+        return answer.toLowerCase() === properAnswer;
+    }
+
+    function pazdanFight(pazdanQuestion, pazdanAnswer) {
+        var yellowCardsForPazdan = 0;
+        do {
+            var answer = showQuestion(pazdanQuestion);
+            var isCorrect = checkAnswer(pazdanAnswer, answer);
+            yellowCardsForPazdan++;
+        } while (!isCorrect && yellowCardsForPazdan < 2);
+        var cardFurPazdana = yellowCardsForPazdan < 2 ? "żółtą" : "czerwoną";
+        window.alert("Przepraszamy to Pazdan jest taki nieustępliwy. Za to zagranie otrzymał " + cardFurPazdana + " kartkę ;)")
+    }
+
+    function checkResult(userPoints) {
         var result;
-        switch (points) {
+        switch (userPoints) {
             case 3:
                 result = "Jesteś najlepszy!";
                 break;
