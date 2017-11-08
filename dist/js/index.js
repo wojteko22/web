249c94088b901f48ba8601288ef737bdf1528ecc
Wojtek Okoński <wojteko22.96@gmail.com>
2017-11-08 13:41:26 +0100

    6 Add eslint

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,45 +1,45 @@
-var miliInMonth = 2629746000;
-var miliInDay = 86400000;
-var miliInHour = 3600000;
-var miliInMin = 60000;
-var miliInSec = 1000;
+var miliInMonth = 2629746000
+var miliInDay = 86400000
+var miliInHour = 3600000
+var miliInMin = 60000
+var miliInSec = 1000
 var nextMatchDay = new Date(2017, 10, 10, 20, 45);
 
 (function () {
-    "use strict";
+  'use strict'
 
-    document.writeln("<h1>Oto strona o reprezentacji Polski!</h1>");
+  document.writeln('<h1>Oto strona o reprezentacji Polski!</h1>')
 
-    window.onload = function () {
-        refreshTimeToNextMatch();
-        setInterval(refreshTimeToNextMatch, 500);
-    };
+  window.onload = function () {
+    refreshTimeToNextMatch()
+    setInterval(refreshTimeToNextMatch, 500)
+  }
 
-    function refreshTimeToNextMatch() {
-        var actualDate = new Date();
-        var timeLeftToMatch = convertFromMilliseconds(nextMatchDay - actualDate);
-        document.getElementById("nextMatchCount").innerHTML = " Pozostało: " +
-            timeLeftToMatch.months + " miesiecy " +
-            timeLeftToMatch.days + " dni " +
-            timeLeftToMatch.hours + " godzin " +
-            timeLeftToMatch.minutes + " minut " +
-            timeLeftToMatch.seconds + " sekund";
+  function refreshTimeToNextMatch () {
+    var actualDate = new Date()
+    var timeLeftToMatch = convertFromMilliseconds(nextMatchDay - actualDate)
+    document.getElementById('nextMatchCount').innerHTML = ' Pozostało: ' +
+      timeLeftToMatch.months + ' miesiecy ' +
+      timeLeftToMatch.days + ' dni ' +
+      timeLeftToMatch.hours + ' godzin ' +
+      timeLeftToMatch.minutes + ' minut ' +
+      timeLeftToMatch.seconds + ' sekund'
+  }
+
+  function convertFromMilliseconds (milliseconds) {
+    var time = parseInt(milliseconds)
+    var months = parseInt(time / miliInMonth)
+    var days = parseInt((time - (months * miliInMonth)) / miliInDay)
+    var hours = parseInt((time - months * miliInMonth - days * miliInDay) / miliInHour)
+    var minutes = parseInt((time - months * miliInMonth - days * miliInDay - hours * miliInHour) / miliInMin)
+    var seconds = parseInt((time - months * miliInMonth - days * miliInDay - hours * miliInHour - minutes * miliInMin) / miliInSec)
+
+    return {
+      months: months,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
     }
-
-    function convertFromMilliseconds(milliseconds) {
-        var time = parseInt(milliseconds);
-        var months = parseInt(time / miliInMonth);
-        var days = parseInt((time - (months * miliInMonth)) / miliInDay);
-        var hours = parseInt((time - months * miliInMonth - days * miliInDay) / miliInHour);
-        var minutes = parseInt((time - months * miliInMonth - days * miliInDay - hours * miliInHour) / miliInMin);
-        var seconds = parseInt((time - months * miliInMonth - days * miliInDay - hours * miliInHour - minutes * miliInMin) / miliInSec);
-
-        return {
-            months: months,
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds
-        };
-    }
-}());
+  }
+}())
--- dist/js/memes.js
+++ dist/js/memes.js
@@ -1,115 +1,114 @@
 (function () {
-    "use strict";
+  'use strict'
 
-    window.addEventListener("load", start);
+  window.addEventListener('load', start)
 
-    function start() {
-        var memeButton = document.getElementById("newMemeButton");
-        memeButton.addEventListener("click", setMeme);
-        var quizButton = document.getElementById("quizButton");
-        quizButton.addEventListener("click", startQuiz);
-        var bmiButton = document.getElementById("bmiButton");
-        bmiButton.addEventListener("click", countBmi);
+  function start () {
+    var memeButton = document.getElementById('newMemeButton')
+    memeButton.addEventListener('click', setMeme)
+    var quizButton = document.getElementById('quizButton')
+    quizButton.addEventListener('click', startQuiz)
+    var bmiButton = document.getElementById('bmiButton')
+    bmiButton.addEventListener('click', countBmi)
+  }
+
+  function setMeme () {
+    var memeImg = document.getElementById('meme')
+    var random = Math.floor(1 + Math.random() * 10)
+    memeImg.setAttribute('src', '../resources/memes/meme' + random + '.jpg')
+  }
+
+  function startQuiz () {
+    var questions = [
+      'W jakim mieście siedzibę ma klub Lewandowskiego?',
+      'Podaj nazwisko piłkarza o ksywce "Pirania"',
+      'Jak ma na imię żona Wojciecha Szczęsnego?'
+    ]
+    var answers = ['monachium', 'pazdan', 'marina']
+    var points = 0
+    for (var i = 0; i < questions.length; i++) {
+      var answer = showQuestion(questions[i])
+      var isCorrect = checkAnswer(answers[i], answer)
+      points += isCorrect ? 1 : 0
+      if (i === 1 && !isCorrect) {
+        var doYouKnownPirania = pazdanFight(questions[i], answers[i])
+        points += doYouKnownPirania ? 1 : 0
+      }
     }
+    checkResult(points)
+  }
 
-    function setMeme() {
-        var memeImg = document.getElementById("meme");
-        var random = Math.floor(1 + Math.random() * 10);
-        memeImg.setAttribute("src", "../resources/memes/meme" + random + ".jpg");
+  function showQuestion (question) {
+    var answer
+    while (!answer) {
+      answer = window.prompt(question)
     }
+    return answer
+  }
 
-    function startQuiz() {
-        var questions = [
-            "W jakim mieście siedzibę ma klub Lewandowskiego?",
-            "Podaj nazwisko piłkarza o ksywce \"Pirania\"",
-            "Jak ma na imię żona Wojciecha Szczęsnego?"
-        ];
-        var answers = ["monachium", "pazdan", "marina"];
-        var points = 0;
-        for (var i = 0; i < questions.length; i++) {
-            var answer = showQuestion(questions[i]);
-            var isCorrect = checkAnswer(answers[i], answer);
-            points += isCorrect ? 1 : 0;
-            if (i === 1 && !isCorrect) {
-                var doYouKnownPirania = pazdanFight(questions[i], answers[i]);
-                points += doYouKnownPirania ? 1 : 0;
-            }
-        }
-        checkResult(points);
+  function checkAnswer (properAnswer, answer) {
+    if (answer.toLowerCase() === properAnswer) {
+      window.alert('Dobra odpowiedź!')
+    } else {
+      window.alert('Zła odpowiedź!')
     }
+    return answer.toLowerCase() === properAnswer
+  }
 
-    function showQuestion(question) {
-        var answer;
-        while (!answer) {
-            answer = window.prompt(question);
-        }
-        return answer;
-    }
+  function pazdanFight (pazdanQuestion, pazdanAnswer) {
+    var yellowCardsForPazdan = 0
+    var isCorrect
+    do {
+      var answer = showQuestion(pazdanQuestion)
+      isCorrect = checkAnswer(pazdanAnswer, answer)
+      yellowCardsForPazdan++
+    } while (!isCorrect && yellowCardsForPazdan < 2)
+    var cardFurPazdana = yellowCardsForPazdan < 2 ? 'żółtą' : 'czerwoną'
+    window.alert('Przepraszamy, to Pazdan jest taki nieustępliwy. Za to zagranie otrzymał ' + cardFurPazdana + ' kartkę ;)')
+    return isCorrect
+  }
 
-    function checkAnswer(properAnswer, answer) {
-        if (answer.toLowerCase() === properAnswer) {
-            window.alert("Dobra odpowiedź!");
-        } else {
-            window.alert("Zła odpowiedź!");
-        }
-        return answer.toLowerCase() === properAnswer;
+  function checkResult (userPoints) {
+    var result
+    switch (userPoints) {
+      case 3:
+        result = 'Jesteś najlepszy!'
+        break
+      case 2:
+        result = 'Nieźle!'
+        break
+      case 1:
+        result = 'Nie za dużo wiesz o reprezentacji'
+        break
+      default:
+        result = 'Doucz się koniecznie!'
     }
+    window.alert(result)
+  }
 
-    function pazdanFight(pazdanQuestion, pazdanAnswer) {
-        var yellowCardsForPazdan = 0;
-        var isCorrect;
-        do {
-            var answer = showQuestion(pazdanQuestion);
-            isCorrect = checkAnswer(pazdanAnswer, answer);
-            yellowCardsForPazdan++;
-        } while (!isCorrect && yellowCardsForPazdan < 2);
-        var cardFurPazdana = yellowCardsForPazdan < 2 ? "żółtą" : "czerwoną";
-        window.alert("Przepraszamy, to Pazdan jest taki nieustępliwy. Za to zagranie otrzymał " + cardFurPazdana + " kartkę ;)");
-        return isCorrect;
+  function countBmi () {
+    var weight = getFloat('Podaj swoją wagę w kilogramach')
+    var height = getFloat('Podaj swój wzrost w metrach')
+    var bmi = Math.round(weight / height / height * 100) / 100
+    var result = 'Twoje BMI to ' + bmi + ', '
+    if (bmi < 22) {
+      result += 'do Lewego trochę Ci brakuje, musisz przypakować!'
+    } else if (bmi < 24) {
+      result += 'jesteś prawie jak Lewy!'
+    } else {
+      result += 'Lewy nie jest taki gruby, musisz schudnąć!'
     }
+    window.alert(result)
+  }
 
-    function checkResult(userPoints) {
-        var result;
-        switch (userPoints) {
-            case 3:
-                result = "Jesteś najlepszy!";
-                break;
-            case 2:
-                result = "Nieźle!";
-                break;
-            case 1:
-                result = "Nie za dużo wiesz o reprezentacji";
-                break;
-            default:
-                result = "Doucz się koniecznie!";
-        }
-        window.alert(result);
+  function getFloat (message) {
+    var str = window.prompt(message)
+    var float = parseFloat(str)
+    var invalid = isNaN(float)
+    if (invalid) {
+      return getFloat(message)
+    } else {
+      return float
     }
-
-    function countBmi() {
-        var weight = getFloat("Podaj swoją wagę w kilogramach");
-        var height = getFloat("Podaj swój wzrost w metrach");
-        var bmi = Math.round(weight / height / height * 100) / 100;
-        var result = "Twoje BMI to " + bmi + ", ";
-        if (bmi < 22) {
-            result += "do Lewego trochę Ci brakuje, musisz przypakować!";
-        } else if (bmi < 24) {
-            result += "jesteś prawie jak Lewy!";
-        } else {
-            result += "Lewy nie jest taki gruby, musisz schudnąć!";
-        }
-        window.alert(result);
-    }
-
-    function getFloat(message) {
-        var str = window.prompt(message);
-        var float = parseFloat(str);
-        var invalid = isNaN(float);
-        if (invalid) {
-            return getFloat(message);
-        }
-        else {
-            return float;
-        }
-    }
-}());
+  }
+}())
