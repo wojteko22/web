b0937788e4a70538c9be06ccf32e5dd046aea54e
Wojtek Okoński <wojteko22.96@gmail.com>
2017-11-09 14:34:16 +0100

    6_2 Review

--- dist/js/collections.js
+++ dist/js/collections.js
@@ -1,46 +1,46 @@
 (function () {
-    'use strict';
+  'use strict'
 
-    document.addEventListener('keypress', showCollectionsSize);
+  document.addEventListener('keypress', showCollectionsSize)
 
-    function showCollectionsSize(e) {
-        e = e || window.event;
-        var key = e.which || e.keyCode;
-        if (key === 109) {
-            window.alert(alertMessage())
-        }
-
-        if (key === 110) {
-            markSpecyficLinkName()
-        }
-
-        if (key === 98) {
-            markSpecyficIndexImage()
-        }
+  function showCollectionsSize (e) {
+    e = e || window.event // todo: czemu w ogóle tu ten window.event (depracated)?
+    var key = e.which || e.keyCode // todo: keyCode jest depracated, na liście jest, ale tu po co w ogóle?
+    if (key === 109) { // todo: co to za klawisze? coś na numerycznej? bo mi nie działają (może trzeba włączyć, nie umiem), komentarz by się przydał
+      window.alert(alertMessage())
     }
 
-    function alertMessage() {
-        return "images: " + document.images.length +
-            " links: " + document.links.length +
-            " anchors: " + document.anchors.length +
-            " forms: " + document.forms.length
+    if (key === 110) {
+      markSpecyficLinkName()
     }
 
-    function markSpecyficLinkName() {
-        var name = window.prompt('Podaj nazwe');
-        var element = document.links.namedItem(name);
-        if (element)
-            element.style.backgroundColor = 'red';
+    if (key === 98) {
+      markSpecyficIndexImage()
     }
-    
-    function markSpecyficIndexImage() {
-        var index = window.prompt('Podaj index obrazka');
-        var image = document.images.item(index);
-        if (image) {
-            image.style.borderBottomStyle = 'solid';
-            image.style.borderBottomColor = 'red';
-            image.style.borderBottomWidth = 'medium';
-        }
-    }
+  }
 
-}());
+  function alertMessage () {
+    return 'images: ' + document.images.length +
+      ' links: ' + document.links.length +
+      ' anchors: ' + document.anchors.length +
+      ' forms: ' + document.forms.length
+  }
+
+  function markSpecyficLinkName () {
+    var name = window.prompt('Podaj nazwę')
+    var element = document.links.namedItem(name)
+    if (element) {
+      element.style.backgroundColor = 'red'
+    }
+  }
+
+  function markSpecyficIndexImage () {
+    var index = window.prompt('Podaj index obrazka')
+    var image = document.images.item(index)
+    if (image) {
+      image.style.borderBottomStyle = 'solid'
+      image.style.borderBottomColor = 'red'
+      image.style.borderBottomWidth = 'medium'
+    }
+  }
+}())
