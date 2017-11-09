0260099d25e2c833d5d279663dd74d699c0d3c6f
Wojtek Okoński <wojteko22.96@gmail.com>
2017-11-09 07:32:26 +0100

    6_5 Add listeners to form

---
+++ dist/js/form.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict'
+
+  window.addEventListener('load', start)
+
+  function start () {
+    registerInputListeners()
+    registerFormListeners()
+  }
+
+  function registerInputListeners () {
+    registerListeners('name', 'Podaj dwa słowa oddzielone spacją')
+    registerListeners('email', 'Podaj email w formacie nazwa@domena')
+    registerListeners('comment', 'Napisz to, co chcesz nam przekazać')
+  }
+
+  function registerListeners (id, text) {
+    var object = document.getElementById(id)
+    var messageBox = document.getElementById('message')
+    object.addEventListener('focus', function () { messageBox.innerHTML = text })
+    object.addEventListener('blur', function () { messageBox.innerHTML = text })
+  }
+
+  function registerFormListeners () {
+    var form = document.getElementById('form')
+    form.addEventListener('submit', function (e) { confirm(e, 'Czy na pewno chcesz wysłać dane mailem?') })
+    form.addEventListener('reset', function (e) { confirm(e, 'Czy na pewno chcesz wyczyścić formularz?') })
+  }
+
+  function confirm (e, message) {
+    var isConfirmed = window.confirm(message)
+    if (!isConfirmed) {
+      e.preventDefault()
+    }
+  }
+}())
