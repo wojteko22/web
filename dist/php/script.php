ad41ec002c5d6e670f5129830dbbb583d14babba
Wojtek Okoński <wojteko22.96@gmail.com>
2017-11-25 12:10:49 +0100

    8_1 Add dynamic typing

--- dist/php/script.php
+++ dist/php/script.php
@@ -4,19 +4,28 @@
     <title>Result of submitting form</title>
 </head>
 <body>
-<p>
+<?php
+$name = $_POST["name"];
+$nameOk = preg_match("/^[a-zA-z]+ [a-zA-z]+$/", $name);
+if (!$nameOk)
+    print("<p>Imię i nazwisko to dwa wyrazy składające się wyłącznie z liter!</p>");
+$email = $_POST["email"];
+$emailOk = preg_match("/^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/", $name);
+if (!$nameOk)
+    print("<p>Wprowadzono niepoprawny email :(</p>");
+print("<p>BTW, o to trochę możliwości PHP:</p>");
+?>
+<details>
+    <summary>Typowanie dynamiczne</summary>
     <?php
-    $name = $_POST["name"];
-    $nameOk = preg_match("/^[a-zA-z]+ [a-zA-z]+$/", $name);
-    if (!$nameOk)
-        print("<p>Imię i nazwisko to dwa wyrazy składające się wyłącznie z liter!</p>");
-    $email = $_POST["email"];
-    $emailOk = preg_match("/^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/", $name);
-    if (!$nameOk)
-        print("<p>Wprowadzono niepoprawny email :(</p>");
-
-
+    $x = 3;
+    print(
+        '<p>Zadeklarowaliśmy i zainicjowaliśmy zmienną: $x = ' . $x . ';</p>');
+    print('<p>gettype($x) = ' . gettype($x) . '</p>');
+    $x = 'trzy';
+    print('<p>$x = "trzy";</p>');
+    print('<p>gettype($x) = ' . gettype($x) . '</p>');
     ?>
-</p>
+</details>
 </body>
 </html>
