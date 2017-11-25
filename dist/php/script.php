c7674ebc293801fa1d665d48ffa5fa4fcd6ddbca
Wojtek Okoński <wojteko22.96@gmail.com>
2017-11-25 11:54:41 +0100

    8 Add regexes

--- dist/php/script.php
+++ dist/php/script.php
@@ -6,7 +6,16 @@
 <body>
 <p>
     <?php
-    print('siema')
+    $name = $_POST["name"];
+    $nameOk = preg_match("/^[a-zA-z]+ [a-zA-z]+$/", $name);
+    if (!$nameOk)
+        print("<p>Imię i nazwisko to dwa wyrazy składające się wyłącznie z liter!</p>");
+    $email = $_POST["email"];
+    $emailOk = preg_match("/^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/", $name);
+    if (!$nameOk)
+        print("<p>Wprowadzono niepoprawny email :(</p>");
+
+
     ?>
 </p>
 </body>
