81993dffad326460723724ae100772fac560c468
Wojtek Okoński <wojteko22.96@gmail.com>
2017-12-03 19:47:40 +0100

    9_1 Save font-family

--- dist/php/cookies-save.php
+++ dist/php/cookies-save.php
@@ -7,14 +7,17 @@ switch ($_POST["style"]) {
     case "classic":
         setcookie("color", "black", $expirationTime, $path);
         setcookie("background-color", "white", $expirationTime, $path);
+        setcookie("font-family", "Times New Roman, serif", $expirationTime, $path);
         break;
     case "dark":
         setcookie("color", "white", $expirationTime, $path);
         setcookie("background-color", "black", $expirationTime, $path);
+        setcookie("font-family", "Arial, sans-serif", $expirationTime, $path);
         break;
     case "crazy":
         setcookie("color", "pink", $expirationTime, $path);
         setcookie("background-color", "violet", $expirationTime, $path);
+        setcookie("font-family", "Luminari, fantasy", $expirationTime, $path);
         break;
 }
 ?>
