3deaa11bf92ff36f942153c8c1ffc4f0679ea4ee
Wojtek Okoński <wojteko22.96@gmail.com>
2017-12-10 14:27:07 +0100

    10 Use database during logging

--- dist/php/log-in.php
+++ dist/php/log-in.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$passwords = array("dawid" => "ptakiLatajaKluczem", "wojtek" => "okon");
 
 if (!isset($_POST["login"]) || !isset($_POST["password"])) {
     header("Location: ../html/login-panel.php");
@@ -12,15 +11,28 @@ $password = $_POST["password"];
 
 $loginSuccessful = false;
 
-if(array_key_exists($login, $passwords))
-    $loginSuccessful = $passwords[$login] == $password;
+if (!($conn = mysqli_connect("localhost", "rusoko", "abc123")))
+    die("<p>Connection failed: " . $conn->connect_error . "</p>");
+
+if (!mysqli_select_db($conn, "data"))
+    die("<p>Error opening data database: " . mysqli_error($conn) . "</p>");
+
+$sql = "SELECT password FROM people WHERE login = '$login'";
+
+if (!($result = mysqli_query($conn, $sql)))
+    die("<p>Error executing query: " . mysqli_error($conn) . "</p>");
+
+mysqli_close($conn);
+$row = mysqli_fetch_row($result);
+$realPassword = $row[0];
+
+$loginSuccessful = $password == $realPassword;
 
 if ($loginSuccessful) {
     $_SESSION['userLogin'] = $login;
     $destination = $_SESSION['destination'];
     header("Location: $destination");
-}
-else {
+} else {
     $_SESSION['errorMessage'] = "Nieprawidłowy login lub hasło";
     header("Location: ../html/login-panel.php");
 }
