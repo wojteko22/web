6ca37035429cf8c7a7f0ba38c43430a7bf9a76d8
Wojtek Okoński <wojteko22.96@gmail.com>
2018-01-15 14:23:10 +0100

    13 Init site

---
+++ asp3/asp3/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp3.Startup))]
+namespace asp3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
